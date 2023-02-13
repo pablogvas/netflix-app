695ea97965ff2891f6e8411ce4026961c863fe28
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2023-02-13 23:03:01 +0100

    Nutflux

---
+++ src/app/models/mediaInterface.ts
@@ -0,0 +1,8 @@
+export interface MediaInterface {
+    category: string;
+    movies: imageInterface[]
+}
+export interface imageInterface {
+    tittle: string;
+    src: string
+}
---
+++ src/app/models/top10-interface.ts
@@ -0,0 +1,10 @@
+export interface Top10Interface {
+    category: string;
+    movies: moviesInterface[]
+}
+
+export interface moviesInterface {
+    number: string;
+    tittle: string;
+    src: string;
+}
